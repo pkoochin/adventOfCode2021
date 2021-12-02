36c430e27d775a8ed4368ea9e983b7da525d858c
pkoochin <paul.koochin@gmail.com>
2021-12-01 21:24:41 -0600

    day one solutions

---
+++ day1/SonarSweep/DepthChecker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarSweep
+{
+    public static class DepthChecker
+    {
+        public static int GetIncreasesByMeasurement(List<int> measurements)
+        {
+            var prev = measurements.FirstOrDefault();
+            var increases = 0;
+            measurements.ForEach(m =>
+            {
+                if (m > prev)
+                {
+                    increases++;
+                }
+                prev = m;
+            });
+            return increases;
+        }
+
+        public static int GetIncreasesByWindow(List<int> measurements)
+        {
+            var windowSums = new List<int>();
+            for( int i =0; i < measurements.Count - 2 ;i++)
+            {
+                var window = measurements.Skip(i).Take(3).Sum();
+                windowSums.Add(window);
+            }
+            return GetIncreasesByMeasurement(windowSums);
+        }
+    }
+}
---
+++ day1/SonarSweep/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SonarSweep;
+
+string path = "./input.txt";
+var measurements = new List<int>();
+using (StreamReader sr = new StreamReader(path))
+{
+    while (sr.Peek() >= 0)
+    {
+        measurements.Add(int.Parse(sr.ReadLine()));                
+    }
+}
+
+
+Console.WriteLine("Increases by measurement: " + DepthChecker.GetIncreasesByMeasurement(measurements));
+Console.WriteLine("Increases by window: " + DepthChecker.GetIncreasesByWindow(measurements));
